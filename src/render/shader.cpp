e929dbe848080cbdf05e069bedf573a558035c3c
LNDF <landergallastegi@gmail.com>
2022-04-08 12:40:51 +0200

    Material

--- src/render/shader.cpp
+++ src/render/shader.cpp
@@ -2,3 +2,232 @@
 
 Shader::~Shader() {}
 
+Material::~Material() {
+    for (std::pair<std::string, std::pair<int, void*>> p : this->properties) {
+        this->freeProperty(p.second.first, p.second.second);
+    }
+}
+
+void Material::deleteProperty(const std::string& property) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+        this->properties.erase(property);
+    }
+}
+
+int Material::getInt(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::INT || pair.first == MaterialPropertyType::BOOL || pair.first == MaterialPropertyType::ENUM)
+            return *static_cast<int*>(pair.second);
+    }
+    return 0;
+}
+
+float Material::getFloat(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::FLOAT)
+            return *static_cast<float*>(pair.second);
+    }
+    return 0;
+}
+
+double Material::getDouble(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::DOUBLE)
+            return *static_cast<double*>(pair.second);
+    }
+    return 0;
+}
+
+glm::vec2 Material::getVec2(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::VEC2)
+            return *static_cast<glm::vec2*>(pair.second);
+    }
+    return glm::vec2();
+}
+
+glm::vec3 Material::getVec3(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::VEC3 || pair.first == MaterialPropertyType::RGB)
+            return *static_cast<glm::vec3*>(pair.second);
+    }
+    return glm::vec3();
+}
+
+glm::vec4 Material::getVec4(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::VEC4 || pair.first == MaterialPropertyType::RGBA)
+            return *static_cast<glm::vec4*>(pair.second);
+    }
+    return glm::vec4();
+}
+
+glm::mat3 Material::getMat3(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::MAT3)
+            return *static_cast<glm::mat3*>(pair.second);
+    }
+    return glm::mat3();
+}
+
+glm::mat4 Material::getMat4(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::MAT4)
+            return *static_cast<glm::mat4*>(pair.second);
+    }
+    return glm::mat4();
+}
+
+Texture* Material::getTexture(const std::string& property) {
+    if (this->properties.count(property)) {
+        const std::pair<int, void*>& pair = this->properties[property];
+        if (pair.first == MaterialPropertyType::TEXTURE)
+            //TODO: resource management
+            return static_cast<Texture*>(pair.second);
+    }
+    return 0;
+}
+
+void Material::setInt(const std::string& property, int value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    int* ptr = (int*) malloc(sizeof(int));
+    *ptr = value;
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::INT;
+    pair.second = ptr;
+    this->properties[property] = pair;
+}
+
+void Material::setFloat(const std::string& property, float value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    float* ptr = (float*) malloc(sizeof(float));
+    *ptr = value;
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::FLOAT;
+    pair.second = ptr;
+    this->properties[property] = pair;
+}
+
+void Material::setDouble(const std::string& property, double value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    double* ptr = (double*) malloc(sizeof(double));
+    *ptr = value;
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::DOUBLE;
+    pair.second = ptr;
+    this->properties[property] = pair;
+}
+
+void Material::setVec2(const std::string& property, glm::vec2 value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    glm::vec2* ptr = new glm::vec2(value);
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::VEC2;
+    pair.second = ptr;
+    this->properties[property] = pair;
+}
+
+void Material::setVec3(const std::string& property, glm::vec3 value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    glm::vec3* ptr = new glm::vec3(value);
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::VEC3;
+    pair.second = ptr;
+    this->properties[property] = pair;
+}
+
+void Material::setVec4(const std::string& property, glm::vec4 value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    glm::vec4* ptr = new glm::vec4(value);
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::VEC4;
+    pair.second = ptr;
+    this->properties[property] = pair;
+}
+
+void Material::setMat3(const std::string& property, glm::mat3 value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    glm::mat3* ptr = new glm::mat3(value);
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::MAT3;
+    pair.second = ptr;
+    this->properties[property] = pair;
+}
+
+void Material::setMat4(const std::string& property, glm::mat4 value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    glm::mat4* ptr = new glm::mat4(value);
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::MAT4;
+    pair.second = ptr;
+    this->properties[property] = pair;
+}
+
+void Material::setTexture(const std::string& property, Texture* value) {
+    if (this->properties.count(property)) {
+        std::pair<int, void*>& old = this->properties[property];
+        this->freeProperty(old.first, old.second);
+    }
+    //TODO: resource manager
+    std::pair<int, void*> pair;
+    pair.first = MaterialPropertyType::TEXTURE;
+    pair.second = value;
+    this->properties[property] = pair;
+}
+
+void Material::freeProperty(int type, void* property) {
+    if (type == MaterialPropertyType::VEC2) {
+        glm::vec2* v2 = static_cast<glm::vec2*>(property);
+        delete v2;
+    } else if (type == MaterialPropertyType::RGB || type == MaterialPropertyType::VEC3) {
+        glm::vec3* v3 = static_cast<glm::vec3*>(property);
+        delete v3;
+    } else if (type == MaterialPropertyType::RGBA || type == MaterialPropertyType::VEC4) {
+        glm::vec4* v4 = static_cast<glm::vec4*>(property);
+        delete v4;
+    } else if (type == MaterialPropertyType::MAT3) {
+        glm::mat3* m3 = static_cast<glm::mat3*>(property);
+        delete m3;
+    } else if (type == MaterialPropertyType::MAT4) {
+        glm::mat4* m4 = static_cast<glm::mat4*>(property);
+        delete m4;
+    } else if (type == MaterialPropertyType::TEXTURE) {
+        //TODO: resource manager
+    } else {
+        free(property);
+    }
+}
--- src/render/shader.h
+++ src/render/shader.h
@@ -4,6 +4,23 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+#include <glm/glm.hpp>
+
+#include "../texture/texture.h"
+
+enum MaterialPropertyType {
+    INT,
+    FLOAT,
+    DOUBLE,
+    BOOL,
+    RGB, RGBA,
+    VEC2, VEC3, VEC4,
+    MAT3, MAT4,
+    TEXTURE,
+    ENUM
+};
+
+class Material;
 
 class Shader {
     public:
@@ -16,14 +33,32 @@ class Shader {
 class Material {
     public:
         ~Material();
-        Shader* getShader();
+        inline Shader* getShader() const {return this->shader;}
         
-        template<typename T>
-        void setProperty(const std::string& property, T data);
-        T getProperty(const std::string& property);
+        void deleteProperty(const std::string& property);
+        int getInt(const std::string& property);
+        float getFloat(const std::string& property);
+        double getDouble(const std::string& property);
+        glm::vec2 getVec2(const std::string& property);
+        glm::vec3 getVec3(const std::string& property);
+        glm::vec4 getVec4(const std::string& property);
+        glm::mat3 getMat3(const std::string& property);
+        glm::mat4 getMat4(const std::string& property);
+        Texture* getTexture(const std::string& property);
+        void setInt(const std::string& property, int value);
+        void setFloat(const std::string& property, float value);
+        void setDouble(const std::string& property, double value);
+        void setVec2(const std::string& property, glm::vec2 value);
+        void setVec3(const std::string& property, glm::vec3 value);
+        void setVec4(const std::string& property, glm::vec4 value);
+        void setMat3(const std::string& property, glm::mat3 value);
+        void setMat4(const std::string& property, glm::mat4 value);
+        void setTexture(const std::string& property, Texture* value);
     private:
         Shader* shader;
-        std::unordered_map<std::string, void*> properties;
+        std::unordered_map<std::string, std::pair<int, void*>> properties;
+
+        void freeProperty(int type, void* property);
 };
 
 #endif
