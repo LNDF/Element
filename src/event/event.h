e86b1d782d4ffffdf53f62696e56f0b903ad18c7
Lander Gallastegi <landergallastegi@gmail.com>
2022-12-04 18:23:28 +0100

    Fix scene?

--- src/event/event.h
+++ src/event/event.h
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <utility>
+#include <typeindex>
 
 #include <utils/packed_map.h>
 
@@ -9,7 +10,7 @@ namespace engine {
     
     template<typename T>
     struct event_info {
-        using callback_type = std::function<bool(T&)>;
+        using callback_type = std::function<bool(const T&)>;
         T event;
         bool cancelled = false;
         callback_type succes_callback;
@@ -28,7 +29,7 @@ namespace engine {
 
     template<typename T>
     class event_dispatcher : public event_dispatcher_base {
-            using callback_type = std::function<bool(T&)>;
+            using callback_type = std::function<bool(const T&)>;
             using info_type = event_info<T>;
 
             friend class event_manager;
@@ -38,7 +39,7 @@ namespace engine {
             std::vector<callback_type> listeners;
             std::vector<event_info<T>> events;
 
-            inline bool maybe_call_and_cancel(callback_type cb, info_type& info) {
+            inline bool maybe_call_and_cancel(callback_type cb, info_type& info) const {
                 if (info.cancelled) return true;
                 if (!cb(info.event)) {
                     //TODO: check cancel
@@ -77,38 +78,27 @@ namespace engine {
                 events.clear();
             }
 
-            void dispatch(info_type& info) {
-                if (first_listener) {
-                    if (maybe_call_and_cancel(first_listener, info)) {
-                        if (info.cancelled_callback) info.cancelled_callback(info.event);
-                        return;
-                    }
-                }
-                for (callback_type listener : listeners) {
-                    if (maybe_call_and_cancel(listener, info)) {
-                        if (info.cancelled_callback) info.cancelled_callback(info.event);
-                        return;
-                    }
-                }
-                if (default_listener) {
-                    if (maybe_call_and_cancel(default_listener, info)) {
-                        if (info.cancelled_callback) info.cancelled_callback(info.event);
-                        return;
-                    }
-                }
-                if (info.succes_callback) info.succes_callback(info.event);
-            }
-
             virtual ~event_dispatcher() {
                 
             }
 
-            void dispatch(const T& event, info_type::callback_type success = nullptr, info_type::callback_type cancel = nullptr) {
-                return dispatch({event, false, nullptr, nullptr});
-            }
-
-            void dispatch(T&& event, info_type::callback_type success = nullptr, info_type::callback_type cancel = nullptr) {
-                return dispatch({std::move(event), false, nullptr, nullptr});
+            bool dispatch(const T& event) const {
+                if (first_listener) {
+                    if (!first_listener(event)) {
+                        return false;
+                    }
+                }
+                for (callback_type listener : listeners) {
+                    if (!listener(event)) {
+                        return false;
+                    }
+                }
+                if (default_listener) {
+                    if (!default_listener(event)) {
+                        return false;
+                    }
+                }
+                return true;
             }
 
             void register_listener(callback_type listener) {
@@ -116,62 +106,67 @@ namespace engine {
             }
 
             void register_first_listener(callback_type listener) {
-                first_listener = listener;
+                if (!first_listener) first_listener = listener;
             }
 
             void register_default_listener(callback_type listener) {
-                default_listener = listener;
+                if (!default_listener) default_listener = listener;
             }
 
             void add_event_to_dispatch(const T& event, info_type::callback_type success = nullptr, info_type::callback_type cancel = nullptr) {
-                events.emplace_back(event, false, nullptr, nullptr);
+                events.emplace_back(event, false, success, cancel);
             }
 
             void add_event_to_dispatch(T&& event, info_type::callback_type success = nullptr, info_type::callback_type cancel = nullptr) {
-                events.emplace_back(std::move(event), false, nullptr, nullptr);
+                events.emplace_back(std::move(event), false, success, cancel);
             }
     };
 
     class event_manager {
         private:
-            packed_map<std::size_t, event_dispatcher_base*> dispatchers;
+            packed_map<std::type_index, event_dispatcher_base*> dispatchers;
 
             template<typename T>
             event_dispatcher<T>* get_event_dispatcher() {
-                std::size_t hash = typeid(T).hash_code();
-                if (!dispatchers.contains(hash)) {
+                std::type_index t(typeid(T));
+                if (!dispatchers.contains(t)) {
                     event_dispatcher<T>* dispatcher = new event_dispatcher<T>();
-                    dispatchers.try_emplace(hash, dispatcher);
+                    dispatchers.try_emplace(t, dispatcher);
                     return dispatcher;
                 }
-                return reinterpret_cast<event_dispatcher<T>*>(dispatchers.at(hash));
+                return reinterpret_cast<event_dispatcher<T>*>(dispatchers.at(t));
             }
         public:
             ~event_manager();
 
             template<typename T>
-            void register_listener(event_dispatcher<T>::callback_type listener) {
+            inline void register_listener(event_dispatcher<T>::callback_type listener) {
                 get_event_dispatcher<T>()->register_listener(listener);
             }
 
             template<typename T>
-            void register_first_listener(event_dispatcher<T>::callback_type listener) {
+            inline void register_first_listener(event_dispatcher<T>::callback_type listener) {
                 get_event_dispatcher<T>()->register_first_listener(listener);
             }
 
             template<typename T>
-            void register_default_listener(event_dispatcher<T>::callback_type listener) {
+            inline void register_default_listener(event_dispatcher<T>::callback_type listener) {
                 get_event_dispatcher<T>()->register_default_listener(listener);
             }
 
             template<typename T>
-            void post_event(const T& event, event_dispatcher<T>::info_type::callback_type success = nullptr, event_dispatcher<T>::info_type::callback_type cancel = nullptr) {
+            inline void post_event(const T& event, event_dispatcher<T>::info_type::callback_type success = nullptr, event_dispatcher<T>::info_type::callback_type cancel = nullptr) {
                 get_event_dispatcher<T>()->add_event_to_dispatch(event, success, cancel);
             }
 
             template<typename T>
-            void post_event(T&& event, event_dispatcher<T>::info_type::callback_type success = nullptr, event_dispatcher<T>::info_type::callback_type cancel = nullptr) {
+            inline void post_event(T&& event, event_dispatcher<T>::info_type::callback_type success = nullptr, event_dispatcher<T>::info_type::callback_type cancel = nullptr) {
                 get_event_dispatcher<T>()->add_event_to_dispatch(std::move(event), success, cancel);
+            }
+
+            template<typename T>
+            inline bool send_event(const T& event) {
+                return get_event_dispatcher<T>()->dispatch(event);
             }
     };
 }
