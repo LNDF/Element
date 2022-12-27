cfb393087bc373f1d73909f36042f390806802c4
LNDF <landergallastegi@gmail.com>
2022-12-27 19:43:56 +0100

    Fix log format

--- src/core/log.cpp
+++ src/core/log.cpp
@@ -10,8 +10,8 @@ std::shared_ptr<spdlog::logger> log::error_log;
 void log::init_log() {
     console_log = spdlog::stdout_color_mt("console");
     error_log = spdlog::stderr_color_mt("error");
-    console_log->set_pattern("%^[%Y-%m-%d %h:%M:%S.%e %!@%s:%# %l]: %v%$");
-    error_log->set_pattern("%^[%Y-%m-%d %h:%M:%S.%e %!@%s:%# %l]: %v%$");
+    console_log->set_pattern("%^[%Y-%m-%d %H:%M:%S.%e %!@%s:%# %l]: %v%$");
+    error_log->set_pattern("%^[%Y-%m-%d %H:%M:%S.%e %!@%s:%# %l]: %v%$");
 }
 
 #endif
