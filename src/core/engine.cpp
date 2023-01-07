1dadfb6c2fd82f9119ed63f979b7a2d2c5d7f015
LNDF <landergallastegi@gmail.com>
2023-01-08 00:10:03 +0100

    Prepare application

---
+++ src/core/engine.cpp
@@ -0,0 +1,66 @@
+#include "engine.h"
+
+#include <core/log.h>
+#include <event/event.h>
+#include <ui/ui.h>
+
+using namespace element;
+
+bool engine::closed = false;
+engine_settings engine::settings;
+
+bool engine::close_event_listener(const events::close&) {
+    closed = true;
+    return true;
+}
+
+void engine::init() {
+    #ifdef ELM_ENABLE_LOGGING
+    log::init_log();
+    ELM_SET_LOG_LEVEL(ELM_LOG_LEVEL_TRACE); //TEMP
+    #endif
+    ELM_INFO("Element engine version {0} starting...", ELM_VERSION);
+    ELM_INFO("Application {0} version {1}", settings.app_name, settings.app_version);
+    ELM_INFO("Configuring application...");
+    event_manager::register_default_listener<events::close>(close_event_listener);
+    ui::init_backend();
+    ELM_INFO("Configuration done");
+}
+
+void engine::setup_engine(const engine_settings& settings) {
+    engine::settings = settings;
+    init();
+}
+
+void engine::setup_engine(engine_settings&& settings) {
+    engine::settings = std::move(settings);
+    init();
+}
+
+void engine::cleanup_engine() {
+    ELM_INFO("Application will close soon. Cleanning up...");
+    ui::cleanup_backend();
+    event_manager::cleanup();
+}
+
+void engine::start() {
+    ELM_INFO("Starting application...");
+    ui::start_ui();
+}
+
+void engine::tick() {
+    event_manager::send_event<events::update>({0}); //TODO: correct delta time
+    event_manager::dispatch_queued_events();
+}
+
+void engine::stop() {
+    ELM_INFO("Stopping application...");
+    ui::stop_ui();
+}
+
+void engine::execute() {
+    ELM_DEBUG("Applilcation execute called");
+    start();
+    while (!should_close()) tick();
+    stop();
+}
---
+++ src/core/engine.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <event/core_events.h>
+#include <string>
+
+namespace element {
+    struct engine_settings {
+        std::string app_name;
+        std::string app_version;
+    };
+
+    class engine {
+        private:
+            static bool closed;
+            static engine_settings settings;
+
+            static void init();
+
+            static bool close_event_listener(const events::close&);
+        public:
+            static void setup_engine(const engine_settings& settings);
+            static void setup_engine(engine_settings&& settings);
+            static void start();
+            static void tick();
+            static void stop();
+            static void cleanup_engine();
+            static void execute();
+            inline static bool should_close() {return closed;}
+    };
+} // namespace element
