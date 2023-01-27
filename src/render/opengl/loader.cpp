2e58b5c70d29e5924b4b217201467f7957b50b9b
Lander Gallastegi <landergallastegi@gmail.com>
2023-01-27 22:16:32 +0100

    Replace GLEW with GLAD and remove GLFW

--- src/render/opengl/loader.cpp
+++ src/render/opengl/loader.cpp
@@ -1,21 +1,20 @@
 #include "loader.h"
 
 #include <core/log.h>
-#include <GL/glew.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 static bool is_opengl_loaded = false;
 
 void element::opengl_load() {
     if (is_opengl_loaded) return;
     is_opengl_loaded = true;
-    glewExperimental = GL_TRUE;
     ELM_INFO("Setting up OpenGl...");
-    GLenum ret = glewInit();
-    if (ret != GLEW_OK) {
-        ELM_CRITICAL("GLEW could not be started. Error: {}", (const char*) glewGetErrorString(ret));
+    if (!gladLoadGL()) {
+        ELM_ERROR("Couldn't load OpenGL functions.");
         return;
     }
-    ELM_DEBUG("GLEW started.");
+    ELM_DEBUG("Functions loaded with GLAD.");
     ELM_INFO("OpenGL info:");
     ELM_INFO("  Renderer:     {}", (const char*) glGetString(GL_RENDERER));
     ELM_INFO("  Vendor:       {}", (const char*) glGetString(GL_VENDOR));
