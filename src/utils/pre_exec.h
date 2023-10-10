397c3549a168c873d9d704475eaf158c67d0d489
Lander Gallastegi <landergallastegi@gmail.com>
2023-10-10 19:09:23 +0200

    Make ELM_PRE_EXECUTE work with overloaded functions

--- src/utils/pre_exec.h
+++ src/utils/pre_exec.h
@@ -1,17 +1,5 @@
 #pragma once
 
-#include <utility>
-
-namespace element {
-    
-    template<typename T, typename...Args>
-    bool call_and_return_true(T func, Args&&... args) {
-        func(std::forward<Args>(args)...);
-        return true;
-    }
-
-} // namespace element
-
 #define __ELM_PRE_EXECUTE_CREATE_NAMEH(name, ctr) name ## ctr
 #define __ELM_PRE_EXECUTE_CREATE_NAME(name, ctr) __ELM_PRE_EXECUTE_CREATE_NAMEH(name, ctr)
-#define ELM_PRE_EXECUTE(...) static bool __ELM_PRE_EXECUTE_CREATE_NAME(__elm_pre_execute_, __COUNTER__) = element::call_and_return_true(__VA_ARGS__);
+#define ELM_PRE_EXECUTE(fun, ...) static bool __ELM_PRE_EXECUTE_CREATE_NAME(__elm_pre_execute_, __COUNTER__) = ([]() -> bool {fun(__VA_ARGS__); return false;})();
