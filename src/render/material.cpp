8bd137bee4589a52e6f3ffde0e4c92ec8b319963
Lander Gallastegi <landergallastegi@gmail.com>
2023-11-16 18:23:35 +0100

    Update material system

--- src/render/material.cpp
+++ src/render/material.cpp
@@ -10,7 +10,68 @@ static void write_to_buffer(const T& t, render::material_buffer& buffer, std::ui
 }
 
 template<typename T>
-static const T* read_from_buffer(const render::material_buffer& buffer, std::uint32_t offset) {
-    return (const T*) &buffer[offset];
+static void read_from_buffer(T& t, const render::material_buffer& buffer, std::uint32_t offset) {
+    const T* ptr = (const T*) &buffer[offset];
+    t = *ptr;
 }
 
+template<typename T, glm::length_t L, glm::qualifier Q>
+static void write_to_buffer<glm::vec<L, T, Q>>(const glm::vec<L, T, Q>& t, render::material_buffer& buffer, std::uint32_t offset) {
+    for (glm::length_t i = 0; i < L; ++i) {
+        write_to_buffer(t[i], buffer, offset);
+        offset += sizeof(T);
+    }
+}
+
+template<typename T, glm::length_t L, glm::qualifier Q>
+static void read_from_buffer<glm::vec<L, T, Q>>(glm::vec<L, T, Q>& t, render::material_buffer& buffer, std::uint32_t offset) {
+    for (glm::length_t i = 0; i < L; ++i) {
+        read_from_buffer(t[i], buffer, offset);
+        offset += sizeof(T);
+    }
+}
+
+template<typename T, glm::length_t C, glm::length_t R, glm::qualifier Q>
+static void write_to_buffer<glm::mat<C, R, T, Q>>(const glm::mat<C, R, T, Q>& t, render::material_buffer& buffer, std::uint32_t offset, std::uint32_t stride) {
+    for (glm::length_t i = 0; i < C; ++i) {
+        write_to_buffer(t[i], buffer, offset);
+        offset += stride;
+    }
+}
+
+template<typename T, glm::length_t C, glm::length_t R, glm::qualifier Q>
+static void read_from_buffer<glm::mat<C, R, T, Q>>(glm::mat<C, R, T, Q>& t, render::material_buffer& buffer, std::uint32_t offset) {
+    for (glm::length_t i = 0; i < C; ++i) {
+        read_from_buffer(t[i], buffer, offset);
+        offset += stride;
+    }
+}
+
+template<typename T>
+static void write_array_to_buffer(const T* t, std::uint32_t size, render::material_buffer& buffer, std::uint32_t offset, std::uint32_t stride) {
+    for (std::uint32_t i = 0; i < size; ++i) {
+        write_to_buffer(t[i], buffer, offset);
+        offset += stride;
+    }
+}
+
+template<typename T>
+static void read_array_from_buffer(T* t, std::uint32_t size, render::material_buffer& buffer, std::uint32_t offset, std::uint32_t stride) {
+    for (std::uint32_t i = 0; i < size; ++i) {
+        read_from_buffer(t[i], buffer, offset);
+        offset += stride;
+    }
+}
+
+std::pair<render::material_buffer*, render::shader_block_member*> render::material::get_buffer_and_layout(const std::string& name) {
+    auto prop_it = properties.find(name);
+    if (prop_it == properties.end()) return std::make_pair(nullptr, nullptr);
+    std::pair<material_buffer*, render::shader_block_member*> result;
+    if (prop_it->second.buffer_index == material::push_constants_index) {
+        result.first = &push_constants_buffer;
+    } else {
+        result.first = &uniform_buffers[prop_it->second.buffer_index];
+    }
+    result.second = prop_it->second.property;
+    return result;
+}
--- src/render/material.h
+++ src/render/material.h
@@ -6,13 +6,12 @@
 #include <cinttypes>
 #include <vector>
 #include <limits>
+#include <utility>
+#include <optional>
 #include <unordered_map>
 
 namespace element {
     namespace render {
-        
-        static constexpr std::uint32_t push_constants_index = std::numeric_limits<std::uint32_t>::max();
-
         struct material_property {
             std::uint32_t buffer_index;
             shader_block_member* property;
@@ -25,11 +24,15 @@ namespace element {
 
         class material {
             private:
+                static constexpr std::uint32_t push_constants_index = std::numeric_limits<std::uint32_t>::max();
+
                 std::vector<material_buffer> uniform_buffers;
                 material_buffer push_constants_buffer;
                 std::unordered_map<std::string, material_property> properties;
                 pipeline_data* data = nullptr;
                 uuid pipeline_id;
+
+                std::pair<material_buffer*, shader_block_member*> get_buffer_and_layout(const std::string& name);
             public:
                 void init(bool reset_buffers);
 
@@ -45,30 +48,14 @@ namespace element {
                 template<typename T>
                 T get_property(const std::string& name);
 
-                template<typename T>
-                void set_array_property(const std::string& name, const std::vector<T>& vec);
+                std::uint32_t get_array_size(const std::string& name);
+                std::uint32_t get_array2d_size(const std::string& name);
 
                 template<typename T>
-                std::vector<T> get_array_property(const std::string& name);
-
+                void set_property_array(const std::string& name, const T* t);
+                
                 template<typename T>
-                void set_array2d_property(const std::string& name, const std::vector<std::vector<T>>& vec);
-
-                template<typename T>
-                std::vector<std::vector<T>> get_array2d_property(const std::string& name);
-
-                template<glm::length_t L, typename T, glm::qualifier Q>
-                void set_vec_property(const std::string& name, const glm::vec<L, T, Q>& vec);
-
-                template<glm::length_t L, typename T, glm::qualifier Q>
-                glm::vec<L, T, Q> get_vec_property(const std::string& name);
-
-                template<glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
-                void set_mat_property(const std::string& name, const glm::mat<C, R, T, Q>& vec);
-
-                template<glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
-                glm::mat<C, R, T, Q> get_mat_property(const std::string& name);
-
+                void get_property_array(const std::string& name, T* t);
         };
 
     } // namespace render
