ee166be16099d12c59e15a8dec34dcc28e256a3e
Lander Gallastegi <landergallastegi@gmail.com>
2023-11-23 23:22:54 +0100

    Vulkan descriptor allocation

--- src/graphics/vulkan_descriptor..h
+++ src/graphics/vulkan_descriptor..h
@@ -5,8 +5,17 @@
 namespace element {
     namespace vulkan {
         
+        struct descriptor_set {
+            vk::DescriptorSet set;
+            vk::DescriptorPool pool;
+        };
+
         vk::DescriptorSetLayout create_empty_descriptorset_layout();
         vk::DescriptorSetLayout create_descriptorset_layout_from_bindings(const std::vector<vk::DescriptorSetLayoutBinding>& bindings);
+
+        descriptor_set allocate_descriptorset();
+        void free_descriptorset(descriptor_set& set);
+        void clean_descriptorsets();
 
     } // namespace vulkan
 } // namespace element
--- src/graphics/vulkan_descriptor.cpp
+++ src/graphics/vulkan_descriptor.cpp
@@ -1,4 +1,11 @@
 #include "vulkan_descriptor..h"
+#include <vector>
+
+static std::vector<vk::DescriptorPool> descriptor_pools;
+
+static vk::DescriptorPoolSize pool_sizes[] = {
+    {vk::DescriptorType::eUniformBuffer, 512}
+};
 
 using namespace element;
 
@@ -16,4 +23,46 @@ vk::DescriptorSetLayout vulkan::create_descriptorset_layout_from_bindings(const 
     info.bindingCount = bindings.size();
     info.pBindings = bindings.data();
     return device.createDescriptorSetLayout(info);
+}
+vulkan::descriptor_set vulkan::allocate_descriptorset(vk::DescriptorSetLayout& layout) {
+    vk::DescriptorSetAllocateInfo alloc_info;
+    descriptor_set ret;
+    alloc_info.pNext = nullptr;
+    alloc_info.pSetLayouts = &layout;
+    alloc_info.descriptorSetCount = 1;
+    for (const vk::DescriptorPool& pool : descriptor_pools) {
+        try {
+            alloc_info.descriptorPool = pool;
+            ret.set = device.allocateDescriptorSets(alloc_info)[0];
+            ret.pool = pool;
+            return ret;
+        } catch (const vk::FragmentedPoolError&) {
+
+        } catch (const vk::OutOfPoolMemoryError&) {
+
+        }
+    }
+    vk::DescriptorPoolCreateInfo info;
+    info.flags = vk::DescriptorPoolCreateFlags(vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet);
+    info.maxSets = 512;
+    info.pPoolSizes = pool_sizes;
+    info.poolSizeCount = 1;
+    vk::DescriptorPool new_pool = device.createDescriptorPool(info);
+    alloc_info.descriptorPool = new_pool;
+    ret.set = device.allocateDescriptorSets(alloc_info)[0];
+    ret.pool = new_pool;
+    descriptor_pools.push_back(new_pool);
+    return ret;
+}
+
+void vulkan::free_descriptorset(descriptor_set& set) {
+    std::array<vk::DescriptorSet, 1> sets = {set.set};
+    device.freeDescriptorSets(set.pool, sets);
+}
+
+void vulkan::clean_descriptorsets() {
+    for (const vk::DescriptorPool& pool : descriptor_pools) {
+        device.destroyDescriptorPool(pool);
+    }
+    descriptor_pools.clear();
 }
