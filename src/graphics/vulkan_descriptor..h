2f511898841862a49f51f0c7c47a7fbe9220e4c6
Lander Gallastegi <landergallastegi@gmail.com>
2023-11-24 00:25:25 +0100

    Update vulkan_descriptor..h

--- src/graphics/vulkan_descriptor..h
+++ src/graphics/vulkan_descriptor..h
@@ -13,7 +13,7 @@ namespace element {
         vk::DescriptorSetLayout create_empty_descriptorset_layout();
         vk::DescriptorSetLayout create_descriptorset_layout_from_bindings(const std::vector<vk::DescriptorSetLayoutBinding>& bindings);
 
-        descriptor_set allocate_descriptorset();
+        descriptor_set allocate_descriptorset(vk::DescriptorSetLayout& layout);
         void free_descriptorset(descriptor_set& set);
         void clean_descriptorsets();
 
