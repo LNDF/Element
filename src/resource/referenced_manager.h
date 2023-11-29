7a1647a944e5af8fda11b491373c1c1c77916d0a
Lander Gallastegi <landergallastegi@gmail.com>
2023-11-30 00:34:33 +0100

    Referenced resource manager #1

---
+++ src/resource/referenced_manager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace element {
+    namespace render {
+        
+        template<typename T, typename C, typename D, typename G>
+        class referenced_resource_manager {
+
+        };
+
+    } //namespace render
+} // namespace element
