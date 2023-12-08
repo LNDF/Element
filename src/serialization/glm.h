f2bd9e5c8562e2d8dab5c9a7e84e58d7e0137c99
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-08 21:22:09 +0100

    Better GLM vector and matrix serialization

--- src/serialization/glm.h
+++ src/serialization/glm.h
@@ -3,52 +3,26 @@
 #include <glm/glm.hpp>
 #include <cereal/cereal.hpp>
 
+namespace element {
+    namespace __detail {
+        static const char* __serialization_glm_member_names[4] = {"x", "y", "z", "w"};
+    } // namespace __detail
+} // namespace element
+
+
 namespace cereal {
 
     template<class Archive, glm::length_t L, typename T, glm::qualifier Q>
-    void save(Archive& ar, const glm::vec<L, T, Q>& vec) {
-        ar(make_size_tag(static_cast<size_type>(L)));
+    void serialize(Archive& ar, glm::vec<L, T, Q>& vec) {
         for (glm::length_t i = 0; i < L; ++i) {
-            ar(vec[i]);
-        }
-    }
-
-    template<class Archive, glm::length_t L, typename T, glm::qualifier Q>
-    void load(Archive& ar, glm::vec<L, T, Q>& vec) {
-        size_type size;
-        ar(make_size_tag(size));
-        glm::length_t vecsize;
-        if (L < size) {
-            vecsize = L;
-        } else {
-            vecsize = static_cast<glm::length_t>(size);
-        }
-        for (glm::length_t i = 0; i < vecsize; ++i) {
-            ar(vec[i]);
+            ar(make_nvp(element::__detail::__serialization_glm_member_names[i], vec[i]));
         }
     }
 
     template<class Archive, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
-    void save(Archive& ar, const glm::mat<C, R, T, Q>& mat) {
-        ar(make_size_tag(static_cast<size_type>(C)));
+    void serialize(Archive& ar, glm::mat<C, R, T, Q>& mat) {
         for (glm::length_t i = 0; i < C; ++i) {
-            ar(mat[i]);
+            ar(make_nvp(element::__detail::__serialization_glm_member_names[i], mat[i]));
         }
     }
-
-    template<class Archive, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
-    void load(Archive& ar, glm::mat<C, R, T, Q>& mat) {
-        size_type size;
-        ar(make_size_tag(size));
-        glm::length_t cols;
-        if (C < size) {
-            cols = C;
-        } else {
-            cols = static_cast<glm::length_t>(size);
-        }
-        for (glm::length_t i = 0; i < cols; ++i) {
-            ar(mat[i]);
-        }
-    }
-
 }
