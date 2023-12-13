7da39a951da990329642b438850ae55f461cfaee
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-13 15:23:11 +0100

    Serialized enabled status

--- src/serialization/scenegraph/node.h
+++ src/serialization/scenegraph/node.h
@@ -9,6 +9,8 @@
 #include <serialization/vector.h>
 #include <serialization/string.h>
 
+#include <cereal/cereal.hpp>
+
 #define __ELM_SCENEGRAPH_MAKE_STORAGE_NAME(type) "storage_" #type
 
 #define __ELM_SERIALIZATION_REGISTER_NODE_STORAGE(type) \
@@ -25,6 +27,7 @@ namespace cereal {
     void save(Archive& ar, const element::scenegraph::node& node) {
         ar(make_nvp("id", node.get_id()));
         ar(make_nvp("name", node.get_name()));
+        ar(make_nvp("enabled", node.is_enabled()));
         ar(make_nvp("parent", node.get_parent()));
         ar(make_nvp("children", node.get_children()));
     }
@@ -33,14 +36,17 @@ namespace cereal {
     void load(Archive& ar, element::scenegraph::node& node) {
         element::uuid id;
         std::string name;
+        bool enabled;
         element::scenegraph::node_ref parent;
         std::vector<element::scenegraph::node_ref> children;
         ar(make_nvp("id", id));
         ar(make_nvp("name", name));
+        ar(make_nvp("enabled", enabled));
         ar(make_nvp("parent", parent));
         ar(make_nvp("children", children));
         node.__set_id(id);
         node.set_name(std::move(name));
+        node.set_enabled(enabled);
         node.__set_parent(parent);
         node.__set_children(std::move(children));
     }
