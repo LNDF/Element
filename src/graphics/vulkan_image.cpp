7656035e6f59f0546ffe121518441330145d1242
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-17 20:57:54 +0100

    Vulkan image wrapper

---
+++ src/graphics/vulkan_image.cpp
@@ -0,0 +1,25 @@
+#include "vulkan_image.h"
+
+using namespace element;
+
+vulkan::image::image(VmaAllocationCreateFlags alloc_flags) : alloc_flags(alloc_flags) {}
+
+vulkan::image::image(const vk::ImageCreateInfo& info, VmaAllocationCreateFlags alloc_flags) : alloc_flags(alloc_flags) {
+    create_image(info);
+}
+
+void vulkan::image::destroy_image() {
+    if (image_alloc != nullptr) {
+        vmaDestroyImage(allocator, vk_image, image_alloc);
+        image_alloc = nullptr;
+    }
+}
+
+void vulkan::image::create_image(const vk::ImageCreateInfo& info) {
+    VmaAllocationCreateInfo alloc_create_info = {};
+    VmaAllocationInfo alloc_info;
+    alloc_create_info.usage = VMA_MEMORY_USAGE_AUTO;
+    alloc_create_info.flags = alloc_flags;
+    alloc_create_info.priority = 1.0f;
+    vmaCreateImage(allocator, reinterpret_cast<const VkImageCreateInfo*>(&info), &alloc_create_info, reinterpret_cast<VkImage*>(&vk_image), &image_alloc, &alloc_info);
+}
---
+++ src/graphics/vulkan_image.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <graphics/vulkan.h>
+
+namespace element {
+    namespace vulkan {
+        
+        class image {
+            private:
+                VmaAllocation image_alloc = nullptr;
+                vk::Image vk_image;
+                VmaAllocationCreateFlags alloc_flags;
+
+                void destroy_image();
+            public:
+                image(VmaAllocationCreateFlags alloc_flags);
+                image(const vk::ImageCreateInfo& info, VmaAllocationCreateFlags alloc_flags);
+
+                void create_image(const vk::ImageCreateInfo& info);
+
+                inline vk::Image get_image() const {return vk_image;}
+        };
+
+    } // namespace vulkan 
+} // namespace element
