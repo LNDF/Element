5e5fc5c14eb6849200e378c2fb1937717a180486
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-21 13:54:57 +0100

    Prepare renderpass for depth attachment

--- src/render/scene_render.cpp
+++ src/render/scene_render.cpp
@@ -15,22 +15,39 @@ void render::scene_renderer::create_forward_renderpass() {
     color_attachment.stencilLoadOp = vk::AttachmentLoadOp::eDontCare;
     color_attachment.stencilStoreOp = vk::AttachmentStoreOp::eDontCare;
     color_attachment.initialLayout = vk::ImageLayout::eUndefined;
-    color_attachment.finalLayout = vk::ImageLayout::eTransferSrcOptimal;
+    color_attachment.finalLayout = vk::ImageLayout::eColorAttachmentOptimal;
+    //Depth attachment
+    vk::AttachmentDescription depth_attachment;
+    depth_attachment.flags = vk::AttachmentDescriptionFlags();
+    depth_attachment.format = vk::Format::eD32Sfloat;
+    depth_attachment.samples = vk::SampleCountFlagBits::e1;
+    depth_attachment.loadOp = vk::AttachmentLoadOp::eClear;
+    depth_attachment.storeOp = vk::AttachmentStoreOp::eDontCare;
+    depth_attachment.stencilLoadOp = vk::AttachmentLoadOp::eDontCare;
+    depth_attachment.stencilStoreOp = vk::AttachmentStoreOp::eDontCare;
+    depth_attachment.initialLayout = vk::ImageLayout::eUndefined;
+    depth_attachment.finalLayout = vk::ImageLayout::eDepthAttachmentOptimal;
     //Subpass's first attachment reference
     vk::AttachmentReference color_attachment_reference;
     color_attachment_reference.attachment = 0;
     color_attachment_reference.layout = vk::ImageLayout::eColorAttachmentOptimal;
+    //Subpass's second attachment reference
+    vk::AttachmentReference depth_attachment_reference;
+    depth_attachment_reference.attachment = 1;
+    depth_attachment_reference.layout = vk::ImageLayout::eDepthAttachmentOptimal;
     //Subpass
     vk::SubpassDescription subpass;
     subpass.flags = vk::SubpassDescriptionFlags();
     subpass.pipelineBindPoint = vk::PipelineBindPoint::eGraphics;
     subpass.pColorAttachments = &color_attachment_reference;
     subpass.colorAttachmentCount = 1;
+    subpass.pDepthStencilAttachment = &depth_attachment_reference;
     //Renderpass
+    vk::AttachmentDescription attachments[] = {color_attachment, depth_attachment};
     vk::RenderPassCreateInfo renderpass_info;
     renderpass_info.flags = vk::RenderPassCreateFlags();
-    renderpass_info.attachmentCount = 1;
-    renderpass_info.pAttachments = &color_attachment;
+    renderpass_info.attachmentCount = 2;
+    renderpass_info.pAttachments = attachments;
     renderpass_info.subpassCount = 1;
     renderpass_info.pSubpasses = &subpass;
     forward_renderpass = vulkan::device.createRenderPass(renderpass_info);
