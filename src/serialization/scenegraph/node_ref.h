40c2189046fbfc8c34816e7b39cfb5340536ac66
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-22 21:30:13 +0100

    Derived node ref serialization

--- src/serialization/scenegraph/node_ref.h
+++ src/serialization/scenegraph/node_ref.h
@@ -39,4 +39,35 @@ namespace cereal {
         ar.loadBinary(id.bytes, 16);
         ref.set_id(id);
     }
+
+    template <class Archive, typename T,
+        cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline std::string save_minimal(const Archive&, const element::scenegraph::node_ref_derived<T>& ref) {
+        return ref.get_id().str(); 
+    }
+    
+    template <class Archive, typename T,
+        cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline  void load_minimal(const Archive&, element::scenegraph::node_ref_derived<T>& ref, const std::string& s) {
+        element::uuid id(s);
+        ref.set_id(id);
+    }
+
+    template <class Archive, typename T,
+        cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline void save(Archive& ar, const element::scenegraph::node_ref_derived<T>& ref) {
+        ar.saveBinary(ref.get_id().bytes, 16);
+    }
+
+    template <class Archive, typename T,
+        cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline void load(Archive& ar, element::scenegraph::node_ref_derived<T>& ref) {
+        element::uuid id;
+        ar.loadBinary(id.bytes, 16);
+        ref.set_id(id);
+    }
 } // namespace cereal
