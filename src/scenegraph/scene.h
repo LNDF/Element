01d44318c623cf1e55099e3b40367906ba53d31c
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-22 21:28:00 +0100

    Add default camera to scene

--- src/scenegraph/scene.h
+++ src/scenegraph/scene.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <scenegraph/node_ref.h>
+#include <scenegraph/nodes/camera_node_ref.h>
 #include <scenegraph/node_storage.h>
 #include <unordered_map>
 #include <memory>
@@ -16,6 +17,7 @@ namespace element {
                 uuid id;
                 std::unordered_map<std::type_index, std::unique_ptr<node_storage_base>> node_storages;
                 node_ref root_node = nullptr;
+                camera_node_ref default_camera = nullptr;
             public:
                 scene();
                 ~scene();
@@ -31,6 +33,9 @@ namespace element {
                 inline const uuid& get_id() const {return id;}
                 inline const node_ref& get_root_node() const {return root_node;}
                 inline const std::unordered_map<std::type_index, std::unique_ptr<node_storage_base>>& get_node_storages() const {return node_storages;}
+
+                inline const camera_node_ref& get_default_camera() const {return default_camera;}
+                inline void set_default_camera(const camera_node_ref& default_camera) {this->default_camera = default_camera;}
 
                 inline void __set_root_node(const node_ref& root_node) {if (id.is_null()) this->root_node = root_node;}
                 inline void __set_node_storages(std::unordered_map<std::type_index, std::unique_ptr<node_storage_base>>&& storages) {
--- src/serialization/scenegraph/scene.h
+++ src/serialization/scenegraph/scene.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <scenegraph/scene.h>
 #include <scenegraph/node_ref.h>
+#include <scenegraph/nodes/camera_node_ref.h>
 #include <serialization/scenegraph/node_ref.h>
 #include <serialization/scenegraph/node.h>
 #include <serialization/scenegraph/node_storage.h>
@@ -36,17 +37,21 @@ namespace cereal {
     template<typename Archive>
     void save(Archive& ar, const element::scenegraph::scene& scene) {
         ar(make_nvp("root_node", scene.get_root_node()));
+        ar(make_nvp("default_camera", scene.get_default_camera()));
         ar(make_nvp("nodes", scene.get_node_storages()));
     }
 
     template<typename Archive>
     void load(Archive& ar, element::scenegraph::scene& scene) {
         element::scenegraph::node_ref root_node;
+        element::scenegraph::camera_node_ref default_camera;
         std::unordered_map<std::type_index, std::unique_ptr<element::scenegraph::node_storage_base>> nodes;
         ar(make_nvp("root_node", root_node));
+        ar(make_nvp("default_camera", default_camera));
         ar(make_nvp("nodes", nodes));
         scene.__set_root_node(root_node);
         scene.__set_node_storages(std::move(nodes));
+        scene.set_default_camera(default_camera);
     }
 
 } // namespace cereal
