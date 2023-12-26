440508c15bab6b2add1e6e9aa2cb017c91d33008
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-26 19:08:58 +0100

    Revert "[HACK] Don't use save/load_minimal"
    This reverts commit 81041bb9cbf34265f2e13097db553cf5b2c96d90.

--- src/serialization/scenegraph/node_ref.h
+++ src/serialization/scenegraph/node_ref.h
@@ -2,33 +2,72 @@
 
 #include <utils/uuid.h>
 #include <scenegraph/node_ref.h>
+#include <serialization/defs.h>
 #include <serialization/utils/uuid.h>
 
 #include <cereal/access.hpp>
 #include <cereal/details/traits.hpp>
 
 namespace cereal {
-    template <class Archive>
-    void save(Archive& ar, const element::scenegraph::node_ref& ref) {
-        ar(make_nvp("id", ref.get_id()));
+    template <class Archive,
+        cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline std::string save_minimal(const Archive&, const element::scenegraph::node_ref& ref) {
+        return ref.get_id().str(); 
     }
-
-    template <class Archive>
-    void load(Archive& ar, element::scenegraph::node_ref& ref) {
-        element::uuid id;
-        ar(make_nvp("id", id));
+    
+    template <class Archive,
+        cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline  void load_minimal(const Archive&, element::scenegraph::node_ref& ref, const std::string& s) {
+        element::uuid id(s);
         ref.set_id(id);
     }
 
-    template <class Archive, typename T>
-    void save(Archive& ar, const element::scenegraph::node_ref_derived<T>& ref) {
-        ar(make_nvp("id", ref.get_id()));
+    template <class Archive,
+        cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline void save(Archive& ar, const element::scenegraph::node_ref& ref) {
+        ar.saveBinary(ref.get_id().bytes, 16);
     }
 
-    template <class Archive, typename T>
-    void load(Archive& ar, element::scenegraph::node_ref_derived<T>& ref) {
+    template <class Archive,
+        cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline void load(Archive& ar, element::scenegraph::node_ref& ref) {
         element::uuid id;
-        ar(make_nvp("id", id));
+        ar.loadBinary(id.bytes, 16);
+        ref.set_id(id);
+    }
+
+    template <class Archive, typename T,
+        cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline std::string save_minimal(const Archive&, const element::scenegraph::node_ref_derived<T>& ref) {
+        return ref.get_id().str(); 
+    }
+    
+    template <class Archive, typename T,
+        cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline  void load_minimal(const Archive&, element::scenegraph::node_ref_derived<T>& ref, const std::string& s) {
+        element::uuid id(s);
+        ref.set_id(id);
+    }
+
+    template <class Archive, typename T,
+        cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline void save(Archive& ar, const element::scenegraph::node_ref_derived<T>& ref) {
+        ar.saveBinary(ref.get_id().bytes, 16);
+    }
+
+    template <class Archive, typename T,
+        cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline void load(Archive& ar, element::scenegraph::node_ref_derived<T>& ref) {
+        element::uuid id;
+        ar.loadBinary(id.bytes, 16);
         ref.set_id(id);
     }
 } // namespace cereal
--- src/serialization/utils/uuid.h
+++ src/serialization/utils/uuid.h
@@ -6,37 +6,31 @@
 #include <cereal/details/traits.hpp>
 
 namespace cereal {
-    // template <class Archive
-    //     // , cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
-    //     // = cereal::traits::sfinae
-    // >
-    // std::string save_minimal(const Archive&, const element::uuid& id) {
-    //     return id.str(); 
-    // }
+    template <class Archive,
+        cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline std::string save_minimal(const Archive&, const element::uuid& id) {
+        return id.str(); 
+    }
     
-    // template <class Archive
-    //     // , cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
-    //     // = cereal::traits::sfinae
-    // >
-    // void load_minimal(const Archive&, element::uuid& id, const std::string& s) {
-    //     id.set_from_str(s);
-    // }
-
-    template <class Archive
-        // , cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
-        // = cereal::traits::sfinae
-    >
-    void save(Archive& ar, const element::uuid& id) {
-        ar(make_nvp("low", id.low));
-        ar(make_nvp("high", id.high));
+    template <class Archive,
+        cereal::traits::EnableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline  void load_minimal(const Archive&, element::uuid& id, const std::string& s) {
+        id.set_from_str(s);
     }
 
-    template <class Archive
-        // , cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
-        // = cereal::traits::sfinae
-    >
-    void load(Archive& ar, element::uuid& id) {
-        ar(make_nvp("low", id.low));
-        ar(make_nvp("high", id.high));
+    template <class Archive,
+        cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline void save(Archive& ar, const element::uuid& id) {
+        ar.saveBinary(id.bytes, 16);
+    }
+
+    template <class Archive,
+        cereal::traits::DisableIf<cereal::traits::is_text_archive<Archive>::value>
+        = cereal::traits::sfinae>
+    inline void load(Archive& ar, element::uuid& id) {
+        ar.loadBinary(id.bytes, 16);
     }
 } // namespace cereal
