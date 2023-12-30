8cc24ea3d4a8e87f7d64df8869ac5895550e7308
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-30 19:53:07 +0100

    Create additional attributes for matrix columns

--- src/render/pipeline.cpp
+++ src/render/pipeline.cpp
@@ -88,13 +88,16 @@ static vk::PipelineShaderStageCreateInfo create_pipeline_shader_stage(const vk::
 
 static void create_pipeline_vertex_input_state(std::vector<vk::VertexInputAttributeDescription>& attributes, std::vector<vk::VertexInputBindingDescription>& bindings) {
     //Per vertex
-    attributes.reserve(5);
+    attributes.reserve(8);
     attributes.emplace_back(0, 0, vk::Format::eR32G32B32Sfloat, offsetof(render::vertex, position)); //position
     attributes.emplace_back(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(render::vertex, normal)); //normal
     attributes.emplace_back(2, 0, vk::Format::eR32G32B32Sfloat, offsetof(render::vertex, tangent)); //tangent
     attributes.emplace_back(3, 0, vk::Format::eR32G32Sfloat, offsetof(render::vertex, tex_coords)); //tex coords
     //Per instance
-    attributes.emplace_back(4, 0, vk::Format::eR32G32B32A32Sfloat, 0); //model matrix
+    attributes.emplace_back(4, 0, vk::Format::eR32G32B32A32Sfloat, 0); //model matrix (col 1)
+    attributes.emplace_back(6, 0, vk::Format::eR32G32B32A32Sfloat, 0); //model matrix (col 3)
+    attributes.emplace_back(5, 0, vk::Format::eR32G32B32A32Sfloat, 0); //model matrix (col 2)
+    attributes.emplace_back(7, 0, vk::Format::eR32G32B32A32Sfloat, 0); //model matrix (col 4)
     //Bindings
     bindings.reserve(2);
     bindings.emplace_back(0, sizeof(render::vertex), vk::VertexInputRate::eVertex); //vertex data
