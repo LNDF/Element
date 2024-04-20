2f509a3ed52ae75f725491f95b7e3e3efa6dc5f0
Lander Gallastegi <landergallastegi@gmail.com>
2024-04-20 14:10:00 +0200

    Create update event adapter

--- src/core/core_event_adapters.cpp
+++ src/core/core_event_adapters.cpp
@@ -9,4 +9,9 @@ static bool close_event_adapter(events::close& e, void* function) {
     return ((uint32_t(*)())function)();
 }
 
+static bool update_event_adapter(events::update& e, void* function) {
+    return ((uint32_t(*)(double))function)(e.delta_time);
+}
+
 ELM_REGISTER_EVENT_ADAPTER(ELM_EVENT_CORE_CLOSE, events::close, close_event_adapter, event_callback_priority::medium)
+ELM_REGISTER_EVENT_ADAPTER(ELM_EVENT_CORE_UPDATE, events::update, update_event_adapter, event_callback_priority::medium)
